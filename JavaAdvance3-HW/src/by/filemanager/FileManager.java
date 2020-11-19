b400ddff68798a9cfa868b3c7f351335775d191e
SergeyE <elyakovsv@gmail.com>
2020-11-20 01:20:41 +0300

    JavaAdvance3-HW

---
+++ JavaAdvance3-HW/src/by/filemanager/FileManager.java
@@ -0,0 +1,15 @@
+package by.filemanager;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class FileManager {
+
+	
+	public static void main(String[] args) {
+		FManager fileManager = new FManager();
+	}
+
+}
